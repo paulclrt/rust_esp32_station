3b16c4ef855f06b9745f27cefc14d447b9cae2b2
paulclrt <p4ul.claret@gmail.com>
2025-12-30 12:28:49 +0100

    initial commit

---
+++ src/bin/main.rs
@@ -0,0 +1,80 @@
+#![no_std]
+#![no_main]
+#![deny(
+    clippy::mem_forget,
+    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+    holding buffers for the duration of a data transfer."
+)]
+#![deny(clippy::large_stack_frames)]
+
+use esp_hal::clock::CpuClock;
+use esp_hal::main;
+use esp_hal::time::{Duration, Instant};
+use esp_hal::timer::timg::TimerGroup;
+use log::info;
+use esp_radio::wifi::{AccessPointConfig, WifiController, WifiEvent};
+
+
+
+
+
+#[panic_handler]
+fn panic(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+extern crate alloc;
+
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+esp_bootloader_esp_idf::esp_app_desc!();
+
+#[allow(
+    clippy::large_stack_frames,
+    reason = "it's not unusual to allocate larger buffers etc. in main"
+)]
+#[main]
+fn main() -> ! {
+    // generator version: 1.1.0
+    esp_println::logger::init_logger_from_env();
+
+    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 98768);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_rtos::start(timg0.timer0);
+
+
+
+    const ssid: &str  = "ESP32-Network";
+    const password: &str = "DEFAULT";
+    let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
+    let (mut _wifi_controller, _interfaces) =
+        esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
+            .expect("Failed to initialize Wi-Fi controller");
+
+
+    // Wifi access point configuration
+    let accesspoint: AccessPointConfig = AccessPointConfig{ 
+        ssid: ssid,
+        ssid_hidden: false,
+
+    };
+
+    let mut wifi_controller: WifiController = _wifi_controller;
+    _wifi_controller.start(Config::sta).expect("Could no start wifi");
+
+    info!(_interfaces.ap);
+    _wifi_controller.connect();
+
+    loop {
+        info!("Hello world!");
+        let delay_start = Instant::now();
+        while delay_start.elapsed() < Duration::from_millis(500) {}
+    }
+
+    // for inspiration have a look at the examples at 
+    // https://github.com/esp-rs/esp-hal/tree/esp-hal-v~1.0/examples
+}
